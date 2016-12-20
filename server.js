938995b86c29a82be816226e8b36f9411b207864
Rick <fishorb@gmail.com>
2016-12-20 21:49:47 +0000

    success!

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var app = express();
 
 app.get('/', function (req, res) {
 
+    var userAgent = req.headers['user-agent'];
+    var theSoftware = userAgent.split('(')[1].split(')')[0];
+    
+    var theInfo = {
+        "ipaddress": req.headers['x-forwarded-for'],
+        "language": req.headers['accept-language'],
+        "software":theSoftware
+    };
+
+    res.send(theInfo);
 });
 
 app.listen(8080, function () {
